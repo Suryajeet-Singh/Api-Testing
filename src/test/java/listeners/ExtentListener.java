e78f66515bdb2bd5845bea0fd507148d5074ede6
Suryajeet Singh <suryajeet.singh@gmail.com>
2026-02-22 23:09:25 +0530

    mail implementation

---
+++ src/test/java/listeners/ExtentListener.java
@@ -0,0 +1,40 @@
+package listeners;
+
+import com.aventstack.extentreports.*;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import reports.ExtentManager;
+
+public class ExtentListener implements ITestListener {
+
+    private static ExtentReports extent = ExtentManager.getInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest =
+                extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test Passed ✅");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip("Test Skipped");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
