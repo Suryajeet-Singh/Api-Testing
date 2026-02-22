91f25768879aa1744c54bbf2f9fb94f6a69cfab3
Suryajeet Singh <suryajeet.singh@gmail.com>
2026-02-22 22:33:51 +0530

    mvn dependency added

--- src/test/java/api/services/TokenService.java
+++ src/test/java/api/services/TokenService.java
@@ -5,38 +5,6 @@ import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
-//package api.services;
-//
-//import api.utils.ConfigManager;
-//import io.restassured.http.ContentType;
-//import io.restassured.response.Response;
-//
-//import java.util.Map;
-//
-//import static io.restassured.RestAssured.given;
-//
-//public class TokenService {
-//
-//    public static String generateToken() {
-//
-//        Response response =
-//                given()
-//                        .baseUri(ConfigManager.get("base.url"))
-//                        .contentType(ContentType.JSON)
-//                        .body(Map.of(
-//                                "project_id", Integer.parseInt(ConfigManager.get("project.id")),
-//                                "user", ConfigManager.get("username"),
-//                                "password", ConfigManager.get("password")
-//                        ))
-//                        .when()
-//                        .post("/token")
-//                        .then()
-//                        .statusCode(200)
-//                        .extract().response();
-//
-//        return response.jsonPath().getString("access_token");
-//    }
-//}
 public class TokenService {
 
     public static String generateToken() {
